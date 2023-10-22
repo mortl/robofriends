ef9529745fbbb18f6336954298c7f30ead45b524
Martin Benes <martin.benes66@gmail.com>
2023-10-22 00:25:00 -0400

    Updates

---
+++ precache-manifest.329551cee5c5d3068d5666beb4d3ddca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74b0a40c755ff6700b938060d34734e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7ccce8a64179959a7586",
+    "url": "/robofriends/static/css/2.85c36af2.chunk.css"
+  },
+  {
+    "revision": "97b229ea02f3d3ecd6ed",
+    "url": "/robofriends/static/css/main.445b9325.chunk.css"
+  },
+  {
+    "revision": "7ccce8a64179959a7586",
+    "url": "/robofriends/static/js/2.201435ed.chunk.js"
+  },
+  {
+    "revision": "97b229ea02f3d3ecd6ed",
+    "url": "/robofriends/static/js/main.1f6a6ee6.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
